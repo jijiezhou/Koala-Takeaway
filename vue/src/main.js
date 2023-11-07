cbf905263fa9127bb124298f95ff9164ff549102
jijiezhou <jijiez@email.unc.edu>
2023-11-06 20:39:28 -0500

    05 Admin.vue internationalization

--- vue/src/main.js
+++ vue/src/main.js
@@ -6,13 +6,14 @@ import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/css/global.css'
 import '@/assets/css/theme/index.css'
 import request from "@/utils/request";
+import locale from 'element-ui/lib/locale/lang/en'
 
 Vue.config.productionTip = false
 
 Vue.prototype.$request = request
 Vue.prototype.$baseUrl = process.env.VUE_APP_BASEURL
 
-Vue.use(ElementUI, {size: "small"})
+Vue.use(ElementUI, {size: "small", locale})
 
 new Vue({
     router,
--- vue/src/views/manager/Admin.vue
+++ vue/src/views/manager/Admin.vue
@@ -1,25 +1,25 @@
 <template>
   <div>
     <div class="search">
-      <el-input placeholder="请输入账号查询" style="width: 200px" v-model="username"></el-input>
-      <el-button type="info" plain style="margin-left: 10px" @click="load(1)">查询</el-button>
-      <el-button type="warning" plain style="margin-left: 10px" @click="reset">重置</el-button>
+      <el-input placeholder="please enter username" style="width: 200px" v-model="username"></el-input>
+      <el-button type="info" plain style="margin-left: 10px" @click="load(1)">Query</el-button>
+      <el-button type="warning" plain style="margin-left: 10px" @click="reset">Reset</el-button>
     </div>
 
     <div class="operation">
-      <el-button type="primary" plain @click="handleAdd">新增</el-button>
-      <el-button type="danger" plain @click="delBatch">批量删除</el-button>
+      <el-button type="primary" plain @click="handleAdd">Add</el-button>
+      <el-button type="danger" plain @click="delBatch">DeleteBatch</el-button>
     </div>
 
     <div class="table">
       <el-table :data="tableData" strip @selection-change="handleSelectionChange">
         <el-table-column type="selection" width="55" align="center"></el-table-column>
-        <el-table-column prop="id" label="序号" width="70" align="center" sortable></el-table-column>
-        <el-table-column prop="username" label="账号"></el-table-column>
-        <el-table-column prop="name" label="姓名"></el-table-column>
-        <el-table-column prop="phone" label="电话"></el-table-column>
-        <el-table-column prop="email" label="邮箱"></el-table-column>
-        <el-table-column label="头像">
+        <el-table-column prop="id" label="id" width="70" align="center" sortable></el-table-column>
+        <el-table-column prop="username" label="username"></el-table-column>
+        <el-table-column prop="name" label="name"></el-table-column>
+        <el-table-column prop="phone" label="phone"></el-table-column>
+        <el-table-column prop="email" label="email"></el-table-column>
+        <el-table-column label="avatar">
           <template v-slot="scope">
             <div style="display: flex; align-items: center">
               <el-image style="width: 40px; height: 40px; border-radius: 50%" v-if="scope.row.avatar"
@@ -27,11 +27,11 @@
             </div>
           </template>
         </el-table-column>
-        <el-table-column prop="role" label="角色"></el-table-column>
-        <el-table-column label="操作" align="center" width="180">
+        <el-table-column prop="role" label="role"></el-table-column>
+        <el-table-column label="operation" align="center" width="180">
           <template v-slot="scope">
-            <el-button size="mini" type="primary" plain @click="handleEdit(scope.row)">编辑</el-button>
-            <el-button size="mini" type="danger" plain @click="del(scope.row.id)">删除</el-button>
+            <el-button size="mini" type="primary" plain @click="handleEdit(scope.row)">Edit</el-button>
+            <el-button size="mini" type="danger" plain @click="del(scope.row.id)">Delete</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -49,22 +49,21 @@
       </div>
     </div>
 
-
-    <el-dialog title="管理员" :visible.sync="fromVisible" width="40%" :close-on-click-modal="false" destroy-on-close>
+    <el-dialog title="Admin Form" :visible.sync="fromVisible" width="40%" :close-on-click-modal="false" destroy-on-close>
       <el-form :model="form" label-width="100px" style="padding-right: 50px" :rules="rules" ref="formRef">
-        <el-form-item label="用户名" prop="username">
-          <el-input v-model="form.username" placeholder="用户名"></el-input>
+        <el-form-item label="username" prop="username">
+          <el-input v-model="form.username" placeholder="username"></el-input>
         </el-form-item>
-        <el-form-item label="姓名" prop="name">
-          <el-input v-model="form.name" placeholder="姓名"></el-input>
+        <el-form-item label="name" prop="name">
+          <el-input v-model="form.name" placeholder="name"></el-input>
         </el-form-item>
-        <el-form-item label="电话" prop="phone">
-          <el-input v-model="form.phone" placeholder="电话"></el-input>
+        <el-form-item label="phone" prop="phone">
+          <el-input v-model="form.phone" placeholder="phone"></el-input>
         </el-form-item>
-        <el-form-item label="邮箱" prop="birth">
-          <el-input v-model="form.email" placeholder="邮箱"></el-input>
+        <el-form-item label="email" prop="email">
+          <el-input v-model="form.email" placeholder="email"></el-input>
         </el-form-item>
-        <el-form-item label="头像">
+        <el-form-item label="avatar">
           <el-upload
               class="avatar-uploader"
               :action="$baseUrl + '/files/upload'"
@@ -72,17 +71,16 @@
               list-type="picture"
               :on-success="handleAvatarSuccess"
           >
-            <el-button type="primary">上传头像</el-button>
+            <el-button type="primary">Upload Avatar</el-button>
           </el-upload>
         </el-form-item>
       </el-form>
 
       <div slot="footer" class="dialog-footer">
-        <el-button @click="fromVisible = false">取 消</el-button>
-        <el-button type="primary" @click="save">确 定</el-button>
+        <el-button @click="fromVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="save">Confirm</el-button>
       </div>
     </el-dialog>
-
 
   </div>
 </template>
@@ -92,9 +90,9 @@ export default {
   name: "Admin",
   data() {
     return {
-      tableData: [],  // 所有的数据
-      pageNum: 1,   // 当前的页码
-      pageSize: 10,  // 每页显示的个数
+      tableData: [],  // all data
+      pageNum: 1,   // current page
+      pageSize: 10,  // number of items each page
       total: 0,
       username: null,
       fromVisible: false,
@@ -102,7 +100,7 @@ export default {
       user: JSON.parse(localStorage.getItem('xm-user') || '{}'),
       rules: {
         username: [
-          {required: true, message: '请输入账号', trigger: 'blur'},
+          {required: true, message: 'please enter username', trigger: 'blur'},
         ]
       },
       ids: []
@@ -112,15 +110,18 @@ export default {
     this.load(1)
   },
   methods: {
-    handleAdd() {   // 新增数据
-      this.form = {}  // 新增数据的时候清空数据
-      this.fromVisible = true   // 打开弹窗
+    //Add Admin
+    handleAdd() {
+      this.form = {}  // clear form data
+      this.fromVisible = true   // open dialog
     },
-    handleEdit(row) {   // 编辑数据
-      this.form = JSON.parse(JSON.stringify(row))  // 给form对象赋值  注意要深拷贝数据
-      this.fromVisible = true   // 打开弹窗
+    //Edit Admin
+    handleEdit(row) {
+      this.form = JSON.parse(JSON.stringify(row))  // deep copy, set form initial value
+      this.fromVisible = true   // open dialog
     },
-    save() {   // 保存按钮触发的逻辑  它会触发新增或者更新
+    //The logic triggered by the save button will trigger new or updated
+    save() {
       this.$refs.formRef.validate((valid) => {
         if (valid) {
           this.$request({
@@ -128,51 +129,57 @@ export default {
             method: this.form.id ? 'PUT' : 'POST',
             data: this.form
           }).then(res => {
-            if (res.code === '200') {  // 表示成功保存
-              this.$message.success('保存成功')
+            if (res.code === '200') {  // save success
+              this.$message.success('save success')
               this.load(1)
               this.fromVisible = false
             } else {
-              this.$message.error(res.msg)  // 弹出错误的信息
+              this.$message.error(res.msg)  // log error msg
             }
           })
         }
       })
     },
-    del(id) {   // 单个删除
-      this.$confirm('您确定删除吗？', '确认删除', {type: "warning"}).then(response => {
+    //deleteById
+    del(id) {
+      this.$confirm('Are you sure to delete？', 'Confirm delete', {type: "warning"}).then(response => {
         this.$request.delete('/admin/delete/' + id).then(res => {
-          if (res.code === '200') {   // 表示操作成功
-            this.$message.success('操作成功')
+          //delete success
+          if (res.code === '200') {
+            this.$message.success('delete success')
             this.load(1)
           } else {
-            this.$message.error(res.msg)  // 弹出错误的信息
+            this.$message.error(res.msg)  // log error msg
           }
         })
       }).catch(() => {
       })
     },
-    handleSelectionChange(rows) {   // 当前选中的所有的行数据
+    //All row data currently selected
+    handleSelectionChange(rows) {
       this.ids = rows.map(v => v.id)
     },
-    delBatch() {   // 批量删除
+    //delete by batch
+    delBatch() {
       if (!this.ids.length) {
-        this.$message.warning('请选择数据')
+        this.$message.warning('please select data')
         return
       }
-      this.$confirm('您确定批量删除这些数据吗？', '确认删除', {type: "warning"}).then(response => {
+      this.$confirm('Are you sure to delete by batch?', 'Confirm delete', {type: "warning"}).then(response => {
         this.$request.delete('/admin/delete/batch', {data: this.ids}).then(res => {
-          if (res.code === '200') {   // 表示操作成功
-            this.$message.success('操作成功')
+          //delete success
+          if (res.code === '200') {
+            this.$message.success('delete success')
             this.load(1)
           } else {
-            this.$message.error(res.msg)  // 弹出错误的信息
+            this.$message.error(res.msg)  // log error info
           }
         })
       }).catch(() => {
       })
     },
-    load(pageNum) {  // 分页查询
+    //pagination
+    load(pageNum) {
       if (pageNum) this.pageNum = pageNum
       this.$request.get('/admin/selectPage', {
         params: {
@@ -193,7 +200,7 @@ export default {
       this.load(pageNum)
     },
     handleAvatarSuccess(response, file, fileList) {
-      // 把头像属性换成上传的图片的链接
+      // Change the avatar attribute to the link of the uploaded image
       this.form.avatar = response.data
     },
   }
