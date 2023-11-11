3eff7129a145deed346df4bc03a2ed5d56ba1b93
jijiezhou <jijiez@email.unc.edu>
2023-11-10 22:00:27 -0500

    10 Goods

---
+++ springboot/src/main/java/com/example/entity/Goods.java
@@ -0,0 +1,141 @@
+package com.example.entity;
+
+import java.math.BigDecimal;
+
+/**
+ * @Classname Goods
+ * @Description TODO
+ * @Author zjj
+ * @Date 11/10/23 9:53PM
+ */
+public class Goods {
+    private Integer id;
+    /** name */
+    private String name;
+    /** price */
+    private BigDecimal price;
+    /** image */
+    private String img;
+    /** description */
+    private String descr;
+    /** origin material */
+    private String origin;
+    /** flavor */
+    private String taste;
+    /** specification */
+    private String specs;
+    /** date */
+    private String date;
+    /** status */
+    private String status;
+    /** discount */
+    private Double discount;
+    /** business ID */
+    private Integer businessId;
+    /** category ID */
+    private Integer categoryId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getTaste() {
+        return taste;
+    }
+
+    public void setTaste(String taste) {
+        this.taste = taste;
+    }
+
+    public String getSpecs() {
+        return specs;
+    }
+
+    public void setSpecs(String specs) {
+        this.specs = specs;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
+    public Integer getBusinessId() {
+        return businessId;
+    }
+
+    public void setBusinessId(Integer businessId) {
+        this.businessId = businessId;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+}
