1ccc3b124cf4f385eb059ebac89f03eb990ee2a4
jijiezhou <jijiez@email.unc.edu>
2023-11-17 23:08:40 -0500

    test again

--- springboot/src/main/java/com/example/controller/AdminController.java
+++ springboot/src/main/java/com/example/controller/AdminController.java
@@ -14,7 +14,7 @@ import java.util.List;
  **/
 @RestController
 @RequestMapping("/admin")
-public class AdminController {//test
+public class AdminController {
 
     @Resource
     private AdminService adminService;
